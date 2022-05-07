1d28169121aaa92cc22425a4b98727aefdd871fb
dxtshiva <dxt_shiva@hotmail.com>
2022-05-07 09:49:10 +0530

    Binary Tree

---
+++ BSTCreation.java
@@ -0,0 +1,47 @@
+public class BSTCreation {
+    class Node{
+        int data;
+        Node left, right;
+
+        Node(int n){
+            this.data = n;
+            this.left = this.right = null;
+        }
+    }
+
+    Node root ;
+    BSTCreation(int value){
+        root = new Node(value);
+    }
+
+   void add(Node node, int value){
+        if(value<node.data){
+            if(node.left==null){
+                node.left = new Node(value);
+                System.out.println(value+" added to left of "+node.data);
+            }
+            else
+                add(node.left,value);
+        }
+        else if(value>node.data){
+            if(node.right==null){
+                node.right = new Node(value);
+                System.out.println(value+" added to right of "+node.data);
+            }
+            else
+                add(node.right,value);
+        }
+        
+    }
+    public static void main(String[] args) {
+        
+        BSTCreation tree = new BSTCreation(13);
+        tree.add(tree.root, 12);
+        tree.add(tree.root, 10);
+        tree.add(tree.root, 4);
+        tree.add(tree.root,19);
+        tree.add(tree.root, 16);
+        tree.add(tree.root, 9);
+
+    }    
+}
---
+++ BinaryTreeCreation.java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BinaryTreeCreation {
+
+    class Node{
+        int data;
+        Node left, right;
+
+        Node(int n){
+            this.data = n;
+            this.left = this.right = null;
+        }
+    }
+    
+    
+    Node root ;
+    Queue<Node> queue = new LinkedList<>();
+    
+    BinaryTreeCreation(int value){
+        root = new Node(value);
+        queue.add(root);
+    }
+    
+    
+    
+    void add(int value){
+        Node curr = queue.peek();
+        if(curr.left==null) {
+            curr.left = new Node(value);
+            System.out.println(value+" added to left of "+curr.data);
+            queue.add(curr.left);
+        }
+        else if(curr.right==null){
+            curr.right = new Node(value);
+            System.out.println(value+" added to right of "+curr.data);
+            queue.add(curr.right);
+            queue.remove();
+        }   
+    }
+    public static void main(String[] args) {
+
+        BinaryTreeCreation tree = new BinaryTreeCreation(13);
+        tree.add(12);
+        tree.add(10);
+        tree.add(4);
+        tree.add(19);
+        tree.add(16);
+        tree.add(9);
+    }    
+}
